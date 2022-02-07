a327c6c48c0b6b9bc435bc3963a255df57178055
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-02-07 14:48:40 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
 
 // https://deliveroo-back-kevin.herokuapp.com
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(async () => {
+    const response = await fetch("https://deliveroo-back-kevin.herokuapp.com");
+    const data = await response.json();
+    console.log(data);
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello</p>
+      <Header />
+      <p>coucou</p>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="header-container-center">
+        <img className="logo" src="/deliveroo_logo.svg" alt="Logo Deliveroo" />
+      </div>
+      <div className="infos-restaurant">
+        <div className="infos-restaurant-container-center"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
